20c327c1715da24bccbf82d241853a8beffe8db5
ahmed khodair <khodair@digitrans.gov.eg>
2019-07-29 14:32:12 +0200

    d

--- lib/Components/SignUpLink.dart
+++ lib/Components/SignUpLink.dart
@@ -12,7 +12,7 @@ class SignUp extends StatelessWidget {
       ),
       onPressed: (){},
       child: new Text(
-        "Don't have an account? Sign Up",
+        "Don't have an account? Sign U",
         textAlign: TextAlign.center,
         overflow: TextOverflow.ellipsis,
         softWrap: true,
